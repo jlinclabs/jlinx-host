6ce6ef806ff10644e11f3660f02bee0ea30cd50d
Jared Grippe <jared@deadlyicon.com>
2022-08-04 10:31:07 -0700

    removed assert and exported newTmpDir test helper

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -31,7 +31,7 @@ Object.assign(exports, {
 async function createTestnet (t, size = 3) {
   const testnet = await _createTestnet(size, t.teardown)
 
-  const newTmpDir = async () => {
+  testnet.newTmpDir = async () => {
     const { path } = await tmp.dir()
     t.teardown(() => {
       fs.rm(path, { recursive: true })
@@ -45,7 +45,7 @@ async function createTestnet (t, size = 3) {
     while (hosts.length < size) {
       const host = new JlinxHost({
         topic: Buffer.from('_testing_jlinx_host_on_hypercore'),
-        storagePath: await newTmpDir(),
+        storagePath: await testnet.newTmpDir(),
         bootstrap: testnet.bootstrap,
         url: `http://${Vault.generateKey().toString('hex')}.com`,
         keyPair: createSigningKeyPair(),
@@ -91,74 +91,3 @@ async function coreValues (core) {
   return values
 }
 
-// const tape = require('tape')
-// const tmp = require('tmp-promise')
-// const fs = require('node:fs/promises')
-// const HyperDHT = require('@hyperswarm/dht')
-// const Vault = require('jlinx-vault')
-// const { createSigningKeyPair } = require('jlinx-util')
-
-// const JlinxHost = require('../../index.js')
-
-// module.exports.test = function (name, fn, _tape = tape) {
-//   return _tape(name, run)
-//   async function run (t) {
-//     const bootstrappers = []
-//     const nodes = []
-
-//     while (bootstrappers.length < 3) {
-//       bootstrappers.push(new HyperDHT({ ephemeral: true, bootstrap: [] }))
-//     }
-
-//     const bootstrap = []
-//     for (const node of bootstrappers) {
-//       await node.ready()
-//       bootstrap.push({ host: '127.0.0.1', port: node.address().port })
-//     }
-
-//     while (nodes.length < 3) {
-//       const node = new HyperDHT({ ephemeral: false, bootstrap })
-//       await node.ready()
-//       nodes.push(node)
-//     }
-
-//     const tmpDirs = []
-//     const newTmpDir = async () => {
-//       const { path } = await tmp.dir()
-//       const destroy = () => fs.rm(path, { recursive: true })
-//       tmpDirs.push({ path, destroy })
-//       return path
-//     }
-
-//     const jlinxHosts = []
-//     const create = async () => {
-//       const jlinxHost = new JlinxHost({
-//         storagePath: await newTmpDir(),
-//         bootstrap,
-//         url: `http://${Vault.generateKey().toString('hex')}.com`,
-//         keyPair: createSigningKeyPair(),
-//         vaultKey: Vault.generateKey()
-//       })
-//       jlinxHosts.push(jlinxHost)
-//       await jlinxHost.connected()
-//       return jlinxHost
-//     }
-//     await fn(t, create)
-
-//     t.teardown(() => {
-//       destroy(jlinxHosts)
-//       destroy(tmpDirs)
-//       destroy(bootstrappers)
-//       destroy(nodes)
-//     })
-//   }
-// }
-// exports.test.only = (name, fn) => exports.test(name, fn, tape.only)
-// exports.test.skip = (name, fn) => exports.test(name, fn, tape.skip)
-
-// function destroy (...nodes) {
-//   for (const node of nodes) {
-//     if (Array.isArray(node)) destroy(...node)
-//     else node.destroy()
-//   }
-// }
